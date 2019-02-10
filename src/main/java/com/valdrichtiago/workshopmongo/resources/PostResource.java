85cb3d36e59acb22748c7479d66a51204e3d63f1
Tiago Valdrich <tiago.valdrich@hotmail.com>
2019-02-10 20:21:57 -0200

    Query with multiple parameters

--- src/main/java/com/valdrichtiago/workshopmongo/resources/PostResource.java
+++ src/main/java/com/valdrichtiago/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.valdrichtiago.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,19 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String title) {
 		title = URL.decodeParam(title);
 		List<Post> posts = postService.findByTitle(title);
+		return ResponseEntity.ok().body(posts);
+	}
+	
+	@RequestMapping(value="/fullsearch", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate
+	) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> posts = postService.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(posts);
 	}
 	
